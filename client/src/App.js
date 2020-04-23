c6d3038a447cc8dc77b93ca68c0ab2f78d22fd4e
NaJ <naj10000@live.com>
2020-04-22 21:00:32 -0400

    setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
+import UpdateMovieForm from './Movies/UpdateMovieForm';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -35,6 +36,10 @@ const App = () => {
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
       </Route>
+      <Route path="/update-movie/:id" 
+              render={(props)=> <UpdateMovieForm {...props} movieList={movieList} getMovieList={getMovieList}/>}
+      />
+       
     </>
   );
 };
---
+++ client/src/Movies/UpdateMovieForm.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom'
+import axios from 'axios';
+
+const initialItem = {
+    title: '',
+    director: '',
+    metascore: '',
+    stars: []
+}
+
+
+
+export default function UpdateMovieForm(props) {
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+
